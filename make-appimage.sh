e7e4574efdab1d3c378ff22ead117be7566da62b
Link4Electronics <link4electronics@gmail.com>
2026-02-27 17:41:24 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/xiphos.svg
 export DESKTOP=/usr/share/applications/xiphos.desktop
+export STARTUPWMCLASS=xiphos
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
