df065a2dd416844510d8af287d41749674ecf08f
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:38:46 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,16 +11,10 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/xiphos.svg
 export DESKTOP=/usr/share/applications/xiphos.desktop
 export STARTUPWMCLASS=xiphos
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
-export DEPLOY_OPENGL=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/xiphos /usr/bin/xiphos-nav
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
